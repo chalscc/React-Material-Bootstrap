d0ddb60cd469d87042921bc49c52309dcd01389d
chalscc <carlescatalancalabuig@gmail.com>
2023-12-22 21:47:14 +0100

    Fix: Imagen de la TarjetaVehiculo

--- src/vehicles/components/TarjetaVehiculo.jsx
+++ src/vehicles/components/TarjetaVehiculo.jsx
@@ -1,33 +1,64 @@
-import { Card, CardContent, CardMedia, Divider, Grid, Typography } from '@mui/material'
+import { Box, Card, CardContent, CardMedia, Divider, Grid, Typography } from '@mui/material'
 import React from 'react'
 
-export const TarjetaVehiculo = () => {
+
+export const TarjetaVehiculo = ({ ano, cambio, combustible, km, marca, modelo, precioMes, precioOriginal, precioVenta, id }) => {
+
+  console.log(id)
+
   return (
     <Grid item xs={12} sm={6} md={3}>
-          <Card>
-            <CardMedia
-              component="img"
-              height="140"
-              image="ruta-de-tu-imagen1.jpg" // Cambia esto por la ruta de tu imagen
-              alt="Imagen del producto 1"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Título del Producto 1
+      <Card>
+        <CardMedia
+          component="img"
+          height="250"
+          image={`../../assets/coche${id}.jpg`}
+          alt="Imagen del producto 1"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {marca}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {modelo}
+          </Typography>
+          <Divider style={{ margin: '10px 0' }} />
+          <Typography variant="body2">
+            <Box color="error.main" component={'span'} sx={{ textDecoration: 'line-through' }}>
+              {precioOriginal} €
+            </Box>
+            <Box component={'span'}>
+              {precioVenta} €
+            </Box>
+          </Typography>
+          <Divider style={{ margin: '10px 0' }} />
+          <Typography variant="body2" color="text.secondary">
+            Segunda Descripción del Producto 1
+          </Typography>
+          <Grid container spacing={1} justifyContent="space-between">
+            <Grid item xs={3}>
+              <Typography variant="body2" color="text.secondary" style={{ border: '1px solid black', padding: '8px' }}>
+                {ano}
               </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Descripción del Producto 1
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="body2" color="text.secondary" style={{ border: '1px solid black', padding: '8px' }}>
+                {km} km
               </Typography>
-              <Divider style={{ margin: '10px 0' }} />
-              <Typography variant="body2" color="text.primary">
-                Precio: $100
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="body2" color="text.secondary" style={{ border: '1px solid black', padding: '8px' }}>
+                {combustible}
               </Typography>
-              <Divider style={{ margin: '10px 0' }} />
-              <Typography variant="body2" color="text.secondary">
-                Segunda Descripción del Producto 1
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="body2" color="text.secondary" style={{ border: '1px solid black', padding: '8px' }}>
+                {cambio}
               </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>
   )
 }
--- src/vehicles/views/CarShopView.jsx
+++ src/vehicles/views/CarShopView.jsx
@@ -1,31 +1,43 @@
-import React from 'react';
+import { useState } from 'react';
 import { Grid, Card, CardContent, Typography } from '@mui/material';
 import { TarjetaVehiculo } from '../components';
+import { cars } from '../../data/cars';
+
 
 export const CarShopView = () => {
+
+  const [carList, setCarList] = useState(cars);
+
+  console.log(cars)
+
+
   return (
-    <Grid 
-      container 
-      direction='column' 
-      justifyContent='flex-end' 
-      alignItems='center' 
+    <Grid
+      container
+      direction='column'
+      justifyContent='flex-end'
+      alignItems='center'
       style={{ height: '50vh' }} // Esto asegura que el contenedor ocupe la mitad inferior de la pantalla
     >
-      <Grid 
-        container 
-        direction='row' 
-        justifyContent='center' 
-        alignItems='center' 
-        spacing={2} 
+      <Grid
+        container
+        direction='row'
+        justifyContent='center'
+        alignItems='center'
+        spacing={2}
         style={{ width: '100%' }} // Ocupa todo el ancho disponible
       >
-        {/* Tarjeta 1 */}
-        <TarjetaVehiculo/>
-        <TarjetaVehiculo/>
-        <TarjetaVehiculo/>
-        <TarjetaVehiculo/>
 
-        
+        {
+          carList.map(( car ) => (
+            <TarjetaVehiculo 
+            key={ car.id }
+            {...car}
+            />
+          ))
+        }
+
+
       </Grid>
     </Grid>
   );
