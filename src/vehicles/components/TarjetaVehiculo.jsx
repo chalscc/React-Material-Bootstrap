f44727e248da9d1568ffa7472d23c6b47289a64d
chalscc <carlescatalancalabuig@gmail.com>
2023-12-22 20:56:46 +0100

    Modificación de las tarjetas de vehiculo

--- src/vehicles/components/TarjetaVehiculo.jsx
+++ src/vehicles/components/TarjetaVehiculo.jsx
@@ -1,16 +1,30 @@
-import { Card, CardContent, Grid, Typography } from '@mui/material'
+import { Card, CardContent, CardMedia, Divider, Grid, Typography } from '@mui/material'
 import React from 'react'
 
 export const TarjetaVehiculo = () => {
   return (
     <Grid item xs={12} sm={6} md={3}>
           <Card>
+            <CardMedia
+              component="img"
+              height="140"
+              image="ruta-de-tu-imagen1.jpg" // Cambia esto por la ruta de tu imagen
+              alt="Imagen del producto 1"
+            />
             <CardContent>
-              <Typography variant="h5" component="div">
-                Tarjeta 1
+              <Typography gutterBottom variant="h5" component="div">
+                Título del Producto 1
               </Typography>
-              <Typography variant="body2">
-                Contenido de la Tarjeta 1
+              <Typography variant="body2" color="text.secondary">
+                Descripción del Producto 1
+              </Typography>
+              <Divider style={{ margin: '10px 0' }} />
+              <Typography variant="body2" color="text.primary">
+                Precio: $100
+              </Typography>
+              <Divider style={{ margin: '10px 0' }} />
+              <Typography variant="body2" color="text.secondary">
+                Segunda Descripción del Producto 1
               </Typography>
             </CardContent>
           </Card>
